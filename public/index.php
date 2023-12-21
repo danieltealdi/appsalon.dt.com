8781043584084eaeb21dc127d969199f94c4ff5e
Daniel Tealdi <daniel@danieltealdi.com>
2023-12-21 18:08:06 +0100

    first commit

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="">
+  <head>
+    <meta charset="utf-8">
+    <title>appsalon</title>
+  </head>
+  <body>
+    <header></header>
+    <main><h1>appsalon.com</h1></main>
+    <footer></footer>
+  </body>
+</html>

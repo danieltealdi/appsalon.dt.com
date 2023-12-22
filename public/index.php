ac6e99955dd2c29aef92fdb654f980926519dc48
Daniel Tealdi <daniel@danieltealdi.com>
2023-12-22 08:38:14 +0100

    Framework

--- public/index.php
+++ public/index.php
@@ -1,12 +1,13 @@
-<!DOCTYPE html>
-<html lang="">
-  <head>
-    <meta charset="utf-8">
-    <title>appsalon</title>
-  </head>
-  <body>
-    <header></header>
-    <main><h1>appsalon.com</h1></main>
-    <footer></footer>
-  </body>
-</html>
+<?php 
+
+require_once __DIR__ . '/../includes/app.php';
+
+use MVC\Router;
+
+$router = new Router();
+
+
+
+
+// Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
+$router->comprobarRutas();
